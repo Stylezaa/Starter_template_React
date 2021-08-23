79dc932cbe56f6474d0b7b1ba4185d67d2cb1249
DevSin <48406477+Stylezaa@users.noreply.github.com>
2021-08-23 22:07:11 +0700

    Succees_template

---
+++ template/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h1>Template Starter React with Style-Component and React-router-dom</h1>
+    </>
+  );
+}
+
+export default App;
